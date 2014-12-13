5ed714021e413a89afd8e7bdf6067015901cb302
Chia Hsin <chiahsin@Chias-MacBook-Air.local>
2014-12-12 16:47:58 -0800

    add student images

--- client/App/Teacher/teacherCtrl.js
+++ client/App/Teacher/teacherCtrl.js
@@ -8,4 +8,7 @@ TeacherController.$inject = ['$scope', 'teacherFactory'];
 
 function TeacherController ($scope, teacherFactory){
 	$scope.confusedStudents = teacherFactory.confusedStudents;
+	// $scope.change = function(){
+	// 	setTotalStudent($scope.num);
+	// };
 }
