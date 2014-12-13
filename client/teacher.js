3f7678049eb04a7a583dfa592fcb50469c67283e
willsen <william.sentance@gmail.com>
2014-12-12 17:29:43 -0800

    shake effect

--- client/teacher.js
+++ client/teacher.js
@@ -18,10 +18,15 @@ var images = ['img/kingsten.png', 'img/krystal.png', 'img/will.png',
 'img/ash.png', 'img/jon.png', 'img/charlie.png', 'img/Mario-icon.png', 'img/silvia.png'];
 
 for (var i = 0, data = []; i < segments; i++) {
-    data[i] = 0;
+  data[i] = 0;
 }
 
-var margin = {top: 40, right: 40, bottom: 60, left: 40},
+var margin = {
+    top: 40,
+    right: 40,
+    bottom: 60,
+    left: 40
+  },
   width = document.body.offsetWidth - margin.right,
   height = document.body.offsetHeight - margin.top - margin.bottom;
 
@@ -30,13 +35,17 @@ var x = d3.time.scale()
   .range([0, width]);
 
 var y = d3.scale.linear()
-  .domain([0,totalStudents])
+  .domain([0, totalStudents])
   .range([height, 0]);
 
 var line = d3.svg.line()
   .interpolate('basis')
-  .x(function(d, i) { return x(now - (segments - 1 - i) * interval); }) // calculate position once on posting!
-  .y(function(d, i) { return y(d); });
+  .x(function(d, i) {
+    return x(now - (segments - 1 - i) * interval);
+  }) // calculate position once on posting!
+  .y(function(d, i) {
+    return y(d);
+  });
 
 var svg = d3.select('body').append('svg')
   .attr('width', width + margin.left + margin.right)
@@ -51,53 +60,55 @@ var xAxis = svg.append('g')
 
 /* adding y-axis*/
 var yAxis = d3.svg.axis()
-    .scale(y)
-    .orient("left")
-    .ticks(totalStudents/20)
-    .tickSize(-width, 0, 0);
+  .scale(y)
+  .orient("left")
+  .ticks(totalStudents / 20)
+  .tickSize(-width, 0, 0);
 
 
 svg.append('g')
-   .attr("class", "y axis")
-   .attr("transform", "translate( 10,  0)")
-   .call(yAxis);
+  .attr("class", "y axis")
+  .attr("transform", "translate( 10,  0)")
+  .call(yAxis);
 
 /* adding path */
 var path = svg.append('g')
-    .append('path')
-    .attr('class','graphline')
-    .datum(data);
+  .append('path')
+  .attr('class', 'graphline')
+  .datum(data);
 
 /*adding square groups*/
-var squareSize = height/totalStudents; 
+var squareSize = height / totalStudents;
 var globalY = height - squareSize;
 
 
 var squares = svg.append('g')
-    .attr('class', 'squares')
-    .style('fill', 'steelblue');
+  .attr('class', 'squares')
+  .style('fill', 'steelblue');
 
 
-var calculateConfusion = function(array){
+var calculateConfusion = function(array) {
+
 
   if (array) {
     confusionCollection = array;
   }
-  if (confusionCollection.length){
+  if (confusionCollection.length) {
     confused = Math.min(confusionCollection.map(function(confusionObj) {
       var elapsed = (new Date()) - (new Date(confusionObj.createdAt));
-      return (elapsed < 3000) ? 1 : (3000/elapsed);
+      return (elapsed < 3000) ? 1 : (3000 / elapsed);
     }).reduce(function(a, b) {
       return a + b;
     }), totalStudents);
   }
 };
 
-var xPerSec = width/interval*segments;
+var xPerSec = width / interval * segments;
 var squareTime = squareSize * xPerSec;
 // console.log('time',squareTime);
 //if the distance to 1st square is less than one squareSize, decrease y
 var holder = [];
+
 var clipId = 0;
 var popSquare = function(student){
  // var xPos = timediff * xPerSec;
@@ -105,15 +116,17 @@ var popSquare = function(student){
   var xPos = width + margin.right + margin.left;
   var yPos = globalY;
 
-  if(holder.length === 0){
+  if (holder.length === 0) {
     yPos = globalY;
-  }else if(holder.length === 1){
+  } else if (holder.length === 1) {
+
 
     var timediff  = (new Date(student.createdAt).getTime() - new Date(holder[0]).getTime());
     if(timediff < squareTime/3){
       globalY -= squareSize;  
       yPos = globalY;   
     }else{
+
       globalY = height - squareSize;
       yPos = globalY;
     }
@@ -128,19 +141,21 @@ var popSquare = function(student){
         .attr("width", squareSize)
         .attr("height", squareSize);
        
-  
+
   holder.shift();
   holder.push(student.createdAt);
 };
 
-var isThresholdReached = function(array, threshold){
+var isThresholdReached = function(array, threshold) {
   var a = document.getElementsByTagName("audio")[0];
-  
+
   //change background color of screen if confused score is greater than threshold
   if (confused > 5) {
     //play audio ding
     a.play();
-    $('body').animate({background: 'indianred'}, 'slow');
+    $('body').animate({
+      background: 'indianred'
+    }, 'slow');
   };
 }
 
@@ -157,38 +172,49 @@ function update() {
   // slide the x-axis left
   xAxis.call(x.axis)
     .selectAll('text')
-      .attr('y',10)
-      .attr('transform', 'rotate(45)')
-      .style('text-anchor', 'start');
-  
+    .attr('y', 10)
+    .attr('transform', 'rotate(45)')
+    .style('text-anchor', 'start');
+
   //redraw the line
   path
     .attr('d', line)
     .attr('transform', null);
 
-//  slide the line left
+  //  slide the line left
   path.transition()
     .duration(interval)
     .ease('linear')
     .attr('transform', 'translate(' + x(now - (segments - 1) * interval) + ')')
-    .each('end',update);
+    .each('end', update);
 
   // pop the old data point off the front
   data.shift();
- 
+
   d3.selectAll('image').transition()
-    .duration(interval*segments*2)
+    .duration(interval * segments * 2)
     .ease('linear')
-    .attr('transform', 'translate('+ (-width*2.2)+', 0)')
+    .attr('transform', 'translate(' + (-width * 2.2) + ', 0)')
     .each('end', update);
- 
+
 }
 update();
 
-$( document ).ready(function(){
-  $('#confused-icon').on('click', function(event) {
+$(document).ready(function() {
+  $('body').on('click', '#confused', function(event) {
     event.preventDefault();
-    console.log('confused icon clicked')
-    /* Act on the event */
+    $('#confused-icon').addClass('animated shake')
+      .delay(1000)
+      .queue(function(next) {
+        $('#confused-icon')
+          .removeClass('animated shake')
+        next();
+      });
+    
+      $('#confused').fadeOut(400, function() {
+        $('#confused').fadeIn(10000)
+        
+    
+    });
   });
 });
